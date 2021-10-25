a5a6f47429f4b3024404adbf1ef203226ec2ddc3
CameronGoble <camerongoble@gmail.com>
2021-10-24 20:51:36 -0600

    Cleaned frog hopping code.  Additive rather than vector now.

--- animals/frog.lua
+++ animals/frog.lua
@@ -1,71 +1,94 @@
 -- animal: frog
 -- features:
--- frogs are a little larger than flies.
+-- frogs are larger than flies.
 -- frogs make occasional ribbity soundss.
 -- frogs occasionally hop in a forward-ish direction.
+-- frogs tend to go in rapid bursts, but not often
 -- frogs mostly sit still.
 -- frogs will go out of their way for a fly.
 -- frogs tend to go slowly, if at all
 -- frogs bounce against the boundaries.
 
 function spawn_frog(n)
- for i = 1,n do
-  local x,y = flr(rnd(sw)), flr(rnd(sh))
-  -- default fly
-  -- adjust these parameters to make custom frogs
-  local f = {
-   visible = true,
-   color = rnd({3,4,11}), --mostly green, a little brown
-   size = 3,
-   pos = create_vector(x, y),
-   vel = create_vector(0, 0),
-   acc = create_vector(0, 0),
-   maxspeed = rnd(10), --always in pixels per frame
-   -- frogs bounce against the boundaries.
-   -- (see resolve_position())
-   boundary_behavior = "bounce",
-   -- main loop functions
-   draw = _draw_frog,
-   update = _update_frog,
-   -- frogs make occasional ribbity soundss.
-   sfx={idle={patch=10, probability = 600}}
-  }
-  add(world, f)
- end
-   add_caption("+"..n .. " frogs")
+  for i = 1,n do
+    -- frogs tend to go in rapid bursts, but not often
+    local frog_maxspeed = 3
+    local frog_mindistance = 8
+    local x,y = flr(rnd(sw)), flr(rnd(sh))
+    local frog = {
+      visible = true,
+      color = rnd({3,4,11}), --mostly green, a little brown
+      size = 3,
+      pos = create_vector(x, y),
+      vel = create_vector(0, 0),
+      acc = create_vector(0, 0),
+      facing = dir_vecs[rnd(dir)]:copy_vector(),
+      maxspeed = rnd(frog_maxspeed)+frog_maxspeed, --100%-150% px per frame
+      hopdist = rnd(frog_mindistance)+frog_mindistance, -- 100%-150%
+      time_to_recovery = rnd(300),
+      -- frogs bounce against the boundaries.
+      -- (see resolve_position())
+      boundary_behavior = "bounce",
+      -- main loop functions
+      draw = _draw_frog,
+      update = _update_frog,
+      -- frogs make occasional ribbity soundss.
+      sfx={idle={patch=10, probability = 600}}
+    }
+    add(world, frog)
+  end
+  add_caption("+"..n .. " frogs")
 end
 
 function _draw_frog(self)
--- frogs are a little larger than flies.
- circfill(self.pos.x, self.pos.y, self.size, self.color)
+  -- frogs are larger than flies.
+  circfill(self.pos.x, self.pos.y, self.size, self.color)
+end
+
+function hop(self)
+  -- a frog is inspired to hop.
+  -- look toward a space in front of the frog, set as a destination
+  local hop_profile = self.facing:copy_vector()
+  self.start = self.pos:copy_vector()
+  hop_profile:scale_vector(self.hopdist) -- go the distance
+  self.dist_intended = hop_profile:magnitude()
+  local facc = hop_profile:copy_vector()
+  facc:limit(self.maxspeed) -- but not too fast
+  local dest = self.pos:copy_vector()
+  dest:add_vector(hop_profile) -- get the desired position
+  -- note where frog is heading so we can tell when we get there
+  self.destination = dest
+  -- accelerate the front toward the target
+  add_force(self, facc)
+  self.dist_traveled = 0
 end
 
 function _update_frog(self)
-    -- frogs tend to go slowly, if at all
-  local hop = self.vel:magnitude()
-  if hop > 0 then
-    -- come to a quick halt after a burst of acceleration
-    local hop_stop = self.vel:copy_vector()
-    hop_stop:scale_vector(-hop/5)
-    add_force(self, hop_stop)
+  -- The big choice right now is: to hop, or not to hop?
+  -- later we can add eat/not eat, mate/not mate, etc
+  -- or rest if we gotta
+  if self.vel:magnitude() == 0 then
+    -- no motion, so maybe start some?
+    -- frogs mostly sit still.
+    if rnd(30)<=1 then
+      hop(self)
+    else
+      -- frogs make occasional ribbity sounds.
+      -- (not mid-hop, though)
+      play_psfx(self.sfx.idle)
+    end
+  else
+    -- we're in-hop.  Find out how far we've gone and expected to go
+    -- i did this with vector math at first, but it doesn't handle
+    -- walls and collision behviors.  better to just add frame-by-frame
+    self.dist_traveled += self.vel:magnitude()
+    -- if we've gone farther than expected, then -hard stop-.
+    if self.dist_traveled >= self.dist_intended then --we've arrived
+      local stop = self.vel:copy_vector()
+      stop:scale_vector(-1)
+      add_force(self, stop)
+      self.dist_traveled = 0
+      -- later this will probably be replaced with a more clean "arrive" function
+    end
   end
-  if hop < self.maxspeed /3 then
-    -- if frog is slow enough, just come to a complete halt.
-    -- (stops frogs from drifting)
-    self.vel:set_magnitude(0)
-  end
-  -- frogs mostly sit still.
-  -- no motion, so maybe start some?
-  if (hop == 0 and rnd(30)<=1) then
-    -- frogs occasionally hop in a forward-ish direction.
-    -- right now, frogs just hop toward to the uppish.
-    -- later we can add frog headings for local directionality
-    local dv=dir_vecs[rnd({"u", "u", "u", "ul", "ur"})]
-    local fd=dv:copy_vector()
-    local s = rnd(self.maxspeed)   -- frogs tend to go slowly, if at all
-    fd:scale_vector(s)
-    add_force(self, fd)
-  end
--- frogs make occasional ribbity sounds.
-  play_psfx(self.sfx.idle)
 end
